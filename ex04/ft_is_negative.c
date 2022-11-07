fdb0ef0a162cefabc629851eb518c89adf3ca6fa
hdiez01 <116254867+hdiez01@users.noreply.github.com>
2022-11-07 12:19:59 +0100

    42 Urduliz

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,16 @@
+void	ft_is_negative(int	n)
+{
+	char	negative;
+	char	positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+	{
+		write(1, &negative, 1);
+	}
+	else
+	{
+		write(1, &positive, 1);
+	}
+}
---
+++ ex05/ft_print_com.c
@@ -0,0 +1,30 @@
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = '0';
+	while (i < '7')
+	{
+		j = i;
+		while (++j <= '8')
+		{
+			k = j;
+			while (k <= '9')
+			{
+				write(1, &i, 1);
+				write(1, &j, 1);
+				write(1, &k, 1);
+
+				if (i != '7' || j != '8' || k != '9')
+				{
+					write(1, ", ", 2);
+				}
+				k++;
+			}
+			i++;
+		}
+	}
+	
+}
