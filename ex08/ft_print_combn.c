ab8d76b6cc4c6fa0facec4603c4a2c2dbcfdc204
hdiez01 <116254867+hdiez01@users.noreply.github.com>
2022-11-08 08:28:16 +0100

    42 Urduliz

---
+++ ex08/ft_print_combn.c
@@ -0,0 +1,29 @@
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb(int	start, int n)
+{
+	int	i;
+
+	if (n == 0)
+	{
+		ft_putchar(', ');
+		ft_putchar(' ');
+		return ;
+	}
+	i = start + 1;
+	while (1 < 10)
+	{
+		ft_putchar(start + '0');
+		ft_putchar(i + '0');
+		ft_print_comb(i, n - 1);
+		i++;
+	}
+}
+
+void ft_print_combn(int	n)
+{
+	ft_print_comb(0,n);
+}
